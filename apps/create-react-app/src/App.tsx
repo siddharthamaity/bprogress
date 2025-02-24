be9cf82f7606b2348837e88f00939147fc3023c4
Elliot Sutton <elsk8@outlook.fr>
2025-02-24 21:16:40 +0100

    feat(core): add css variables

--- apps/create-react-app/src/App.tsx
+++ apps/create-react-app/src/App.tsx
@@ -7,8 +7,8 @@ function App() {
     <div className="App">
       <h1>React BProgress</h1>
 
-      <button onClick={start}>Start</button>
-      <button onClick={stop}>Stop</button>
+      <button onClick={() => start()}>Start</button>
+      <button onClick={() => stop()}>Stop</button>
       <button onClick={pause}>Pause</button>
       <button onClick={resume}>Resume</button>
     </div>
--- packages/core/src/lib/css.ts
+++ packages/core/src/lib/css.ts
@@ -6,78 +6,89 @@ export type CssOptions = {
   spinnerPosition?: SpinnerPosition;
 };
 
-export const css = ({ color, height, spinnerPosition }: CssOptions) => `
-.bprogress {
-  pointer-events: none;
-  z-index: 99999;
-}
-
-.bprogress .bar {
-  background: ${color};
-
-  position: fixed;
-  z-index: 99999;
-  top: 0;
-  left: 0;
-
-  width: 100%;
-  height: ${height};
-}
-
-/* Fancy blur effect */
-.bprogress .peg {
-  display: block;
-  position: absolute;
-  right: 0px;
-  width: 100px;
-  height: 100%;
-  box-shadow: 0 0 10px ${color}, 0 0 5px ${color};
-  opacity: 1.0;
-
-  -webkit-transform: rotate(3deg) translate(0px, -4px);
-      -ms-transform: rotate(3deg) translate(0px, -4px);
-          transform: rotate(3deg) translate(0px, -4px);
-}
-
-/* Remove these to get rid of the spinner */
-.bprogress .spinner {
-  display: block;
-  position: fixed;
-  z-index: 1031;
-  top: ${
+export const css = ({
+  color = '#29d',
+  height = '2px',
+  spinnerPosition = 'top-right',
+}: CssOptions) => `
+:root {
+  --bprogress-color: ${color};
+  --bprogress-height: ${height};
+  --bprogress-spinner-size: 18px;
+  --bprogress-spinner-animation-duration: 400ms;
+  --bprogress-spinner-border-size: 2px;
+  --bprogress-box-shadow: 0 0 10px ${color}, 0 0 5px ${color};
+  --bprogress-z-index: 99999;
+  --bprogress-spinner-top: ${
     spinnerPosition === 'top-right' || spinnerPosition === 'top-left'
       ? '15px'
       : 'auto'
   };
-  bottom: ${
+  --bprogress-spinner-bottom: ${
     spinnerPosition === 'bottom-right' || spinnerPosition === 'bottom-left'
       ? '15px'
       : 'auto'
   };
-  right: ${
+  --bprogress-spinner-right: ${
     spinnerPosition === 'top-right' || spinnerPosition === 'bottom-right'
       ? '15px'
       : 'auto'
   };
-  left: ${
+  --bprogress-spinner-left: ${
     spinnerPosition === 'top-left' || spinnerPosition === 'bottom-left'
       ? '15px'
       : 'auto'
   };
 }
 
+.bprogress {
+  pointer-events: none;
+  z-index: var(--bprogress-z-index);
+}
+
+.bprogress .bar {
+  background: var(--bprogress-color);
+  position: fixed;
+  z-index: var(--bprogress-z-index);
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: var(--bprogress-height);
+}
+
+/* Fancy blur effect */
+.bprogress .peg {
+  display: block;
+  position: absolute;
+  right: 0;
+  width: 100px;
+  height: 100%;
+  box-shadow: var(--bprogress-box-shadow);
+  opacity: 1.0;
+  transform: rotate(3deg) translate(0px, -4px);
+}
+
+/* Remove these to get rid of the spinner */
+.bprogress .spinner {
+  display: block;
+  position: fixed;
+  z-index: var(--bprogress-z-index);
+  top: var(--bprogress-spinner-top);
+  bottom: var(--bprogress-spinner-bottom);
+  right: var(--bprogress-spinner-right);
+  left: var(--bprogress-spinner-left);
+}
+
 .bprogress .spinner-icon {
-  width: 18px;
-  height: 18px;
+  width: var(--bprogress-spinner-size);
+  height: var(--bprogress-spinner-size);
   box-sizing: border-box;
-
-  border: solid 2px transparent;
-  border-top-color: ${color};
-  border-left-color: ${color};
+  border: solid var(--bprogress-spinner-border-size) transparent;
+  border-top-color: var(--bprogress-color);
+  border-left-color: var(--bprogress-color);
   border-radius: 50%;
-
-  -webkit-animation: bprogress-spinner 400ms linear infinite;
-          animation: bprogress-spinner 400ms linear infinite;
+  -webkit-animation: bprogress-spinner var(--bprogress-spinner-animation-duration) linear infinite;
+          animation: bprogress-spinner var(--bprogress-spinner-animation-duration) linear infinite;
 }
 
 .bprogress-custom-parent {
@@ -94,6 +105,7 @@ export const css = ({ color, height, spinnerPosition }: CssOptions) => `
   0%   { -webkit-transform: rotate(0deg); }
   100% { -webkit-transform: rotate(360deg); }
 }
+
 @keyframes bprogress-spinner {
   0%   { transform: rotate(0deg); }
   100% { transform: rotate(360deg); }
